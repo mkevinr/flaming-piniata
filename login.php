378ce389fab41db77ae9a1432b6a72a81d54b5b2
Ubuntu <ubuntu@ip-10-140-12-154.ec2.internal>
2013-02-01 12:03:49 +0000

    initial project version

---
+++ login.php
@@ -0,0 +1,43 @@
+<?php
+
+$decoded = file_get_contents("users.json");
+$decoded = json_decode($file);
+$decoded = $decoded->users;
+
+$foundUser = true;
+$accessToken
+
+foreach($decoded as $user){
+
+	if($name == $user->name){
+		
+		$foundUser = true;
+		$accessToken = $user->access_token
+		break;
+  	}
+}
+
+if($foundUser){
+
+	
+}
+else{
+	echo "Invalid user name. Please create an account first"
+}
+
+print_r($_REQUEST);
+$name = $_REQUEST['username'];
+if($name ==
+?>
+
+<html>
+<body>
+<form method="POST">
+User name: <input type="text" name="username">
+<input type="submit" value="Submit">
+</form>
+
+<body>
+<html>
+
+
