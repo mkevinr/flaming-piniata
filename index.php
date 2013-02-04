3c9954d3ed6b8f7b953080c3c591a2649b3fae2c
Kevin Murdock <kevinm6400@gmail.com>
2013-02-04 23:44:26 +0000

    Everything working for lab1 but Oauth and API calls

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
-<a href="http://ec2-54-234-155-254.compute-1.amazonaws.com/create_account.php">Create account</a>
+
+<a href="http://ec2-54-234-155-254.compute-1.amazonaws.com/create_account.php">Create Account</a>
+<br>
 <a href="http://ec2-54-234-155-254.compute-1.amazonaws.com/login.php">Login</a>
+<br>
+<a href="/logout.php">Logout</a>
+<br>
+<a href="/users.php">Users</a>
 
 <a href="https://foursquare.com/oauth2/authenticate?client_id=3B53D2V4SEVOHI1R5LNL1H50N4400SQO2JKJSO5MMSP4FLIF&response_type=token&redirect_uri=http://ec2-50-19-20-65.compute-1.amazonaws.com/Oauth.php">Oath</a>
 <a href="https://api.foursquare.com/v2/users/self/checkins?oauth_token=EYVRQAWL5OJAOVVHFFAKYJDSNJVRPOWF0XFGHG3MBWBWEYD3">Checkins</a>
