4036a08d071a87151430dfd12425556dfe923959
Kevin Murdock <kevinm6400@gmail.com>
2013-02-05 16:53:55 +0000

    Actual finished lab 1 CS462

---
+++ profile.php
@@ -0,0 +1,37 @@
+<?php
+
+  session_start();
+
+  $access_token = json_decode(file_get_contents('./users.json'), true);
+  $access_token = $access_token[$_REQUEST['username']];
+  $ch = curl_init("https://api.foursquare.com/v2/users/self/venuehistory?oauth_token=" . $access_token);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_HEADER, 0);
+  $response = json_decode(curl_exec($ch), true);
+  curl_close($ch);
+  $items = $response['response']['venues']['items'];
+
+  echo "<a href=\"/\">Home</a><br>";
+  echo "<a href=\"/users.php\">Back</a><br>";
+  echo "<br><b>Venues:</b><br><br>";
+
+  if($_SESSION['username'] == $_REQUEST['username']){
+
+    foreach($items as $item){
+
+      $venue = $item['venue'];
+      echo "Venue: " . $venue['name'];
+      echo " Location: " . $venue['location']['city'];
+      echo ", " . $venue['location']['state'];
+      echo "<br>";
+    }
+  }
+  else{
+
+    $venue = $items[0]['venue'];
+    echo "Venue: " . $venue['name'];
+    echo " Location: " . $venue['location']['city'];
+    echo ", " . $venue['location']['state'];
+    echo "<br>";
+  }
+?>
