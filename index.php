9318fd94fb0a0baf7f3f238675c69d26af6777aa
Kevin Murdock <kevinm6400@gmail.com>
2013-02-05 01:33:00 +0000

    Almost finished

--- index.php
+++ index.php
@@ -7,6 +7,6 @@
 <br>
 <a href="/users.php">Users</a>
 
-<a href="https://foursquare.com/oauth2/authenticate?client_id=3B53D2V4SEVOHI1R5LNL1H50N4400SQO2JKJSO5MMSP4FLIF&response_type=token&redirect_uri=http://ec2-50-19-20-65.compute-1.amazonaws.com/Oauth.php">Oath</a>
+<a href="https://foursquare.com/oauth2/authenticate?client_id=3B53D2V4SEVOHI1R5LNL1H50N4400SQO2JKJSO5MMSP4FLIF&response_type=token&redirect_uri=http://ec2-54-23-155-254.compute-1.amazonaws.com/Oauth.php">Oauth</a>
 <a href="https://api.foursquare.com/v2/users/self/checkins?oauth_token=EYVRQAWL5OJAOVVHFFAKYJDSNJVRPOWF0XFGHG3MBWBWEYD3">Checkins</a>
 </body> </html>
