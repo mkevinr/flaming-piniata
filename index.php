1040eddb57d0d4cc6ad7cc9937cb38b9c44b43f9
Kevin Murdock <kevinm6400@gmail.com>
2013-02-05 04:27:21 +0000

    final version lab 1?

--- index.php
+++ index.php
@@ -1,12 +1,9 @@
-
-<a href="http://ec2-54-234-155-254.compute-1.amazonaws.com/create_account.php">Create Account</a>
+<p><b>Welcome to Kevin Murdock's App!<br>Please choose an option:</b></p>
+<a href="/create_account.php">Create Account</a>
 <br>
-<a href="http://ec2-54-234-155-254.compute-1.amazonaws.com/login.php">Login</a>
+<a href="/login.php">Login</a>
 <br>
 <a href="/logout.php">Logout</a>
 <br>
 <a href="/users.php">Users</a>
-
-<a href="https://foursquare.com/oauth2/authenticate?client_id=3B53D2V4SEVOHI1R5LNL1H50N4400SQO2JKJSO5MMSP4FLIF&response_type=token&redirect_uri=http://ec2-54-23-155-254.compute-1.amazonaws.com/Oauth.php">Oauth</a>
-<a href="https://api.foursquare.com/v2/users/self/checkins?oauth_token=EYVRQAWL5OJAOVVHFFAKYJDSNJVRPOWF0XFGHG3MBWBWEYD3">Checkins</a>
 </body> </html>
--- users.php
+++ users.php
@@ -8,7 +8,7 @@ $decoded = json_decode($decoded, true);
 
 foreach($decoded as $user => $access_token){
 
-    echo "<a href=\"http://ec2-54-234-155-254.compute-1.amazonaws.com/profile.php?username=" . $user . "\">" . $user . "</a><br>";
+    echo "<a href=\"/profile.php?username=" . $user . "\">" . $user . "</a><br>";
 }
 ?>
 
