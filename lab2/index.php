c82827c0b661aca0944a2380452f89f18f8c2acf
Kevin Murdock <kevinm6400@gmail.com>
2013-03-14 03:32:48 +0000

    Starting lab 3

---
+++ lab2/index.php
@@ -0,0 +1,9 @@
+<p><b>Welcome to Kevin Murdock's App!<br>Please choose an option:</b></p>
+<a href="/create_account.php">Create Account</a>
+<br>
+<a href="/login.php">Login</a>
+<br>
+<a href="/logout.php">Logout</a>
+<br>
+<a href="/users.php">Users</a>
+</body> </html>
---
+++ lab2/login.php
@@ -0,0 +1,58 @@
+
+<?php
+
+if(strlen($_REQUEST['username']) > 0){
+
+
+  $con = mysql_connect("localhost","root","altair8");
+
+  if (!$con)
+  {
+    die('Could not connect: ' . mysql_error());
+  }
+
+  mysql_select_db("master", $con);
+
+  $result = mysql_query("SELECT username, privileges FROM USERS");
+
+  while($row = mysql_fetch_array($result)){
+
+      if($row['username'] == $_REQUEST['username']){
+
+        session_start();
+	$_SESSION['username'] = $_REQUEST['username'];
+	
+	if($row['privileges'] == "admin"){
+
+	  $_SESSION['privileges'] = "admin";
+	  header("Location: admin_profile.php");
+	}
+        else if($row['privileges'] == "driver"){
+
+	  $_SESSION['privileges'] = "driver";
+	  header("Location: driver_profile.php");
+	}
+    }
+  }
+}
+
+if(strlen($_REQUEST['username']) > 0){
+
+	echo "Invalid user name. Please create an account first";
+}
+?>
+
+
+<html>
+<body>
+<a href="/">Home</a><br><br>
+<b>Login:</b><br><br>
+<form action="/login.php" method="POST">
+User name: <input type="text" name="username">
+<input type="submit" value="Login">
+</form>
+
+<body>
+<html>
+
+
