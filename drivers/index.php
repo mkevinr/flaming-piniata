8aa07de1df44549408e4a517911386d420e4fdaf
unknown <kevinm6400@gmail.com>
2013-03-15 13:36:21 -0600

    Altered driver home page

--- drivers/index.php
+++ drivers/index.php
@@ -1,5 +1,12 @@
 <html>
 <body>
-<p>Driver Home</p>
+<p><b>Drivers Home</b></p><br><br>
+
+<?php
+	$server_address = file_get_contents("../server_address");
+	echo "<a href=\"http://" . $server_address . "/drivers/login.php\"><b>Login</b></a><br>";
+	echo "<a href=\"http://" . $server_address . "/drivers/create_account.php\"><b>Create Account</b></a><br>";
+	echo "<a href=\"http://" . $server_address . "/drivers/driver_profile.php\"><b>Profile</b></a><br>";
+?>
 </body>
 </html>
