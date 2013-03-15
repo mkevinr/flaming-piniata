ae482b085fc5fa801868d38001abcba7c1f8a464
unknown <kevinm6400@gmail.com>
2013-03-15 01:30:47 -0600

    fixed oauth for account creation of drivers and added a home page for drivers on the driver site

--- drivers/oauth.php
+++ drivers/oauth.php
@@ -7,8 +7,6 @@
 	
 	$server_address = file_get_contents("../server_address");
 	
-	echo "server_address: " . server_address;
-	
 	$redirect_location = "Location: https://foursquare.com/oauth2/authenticate?"
 			. "client_id=3B53D2V4SEVOHI1R5LNL1H50N4400SQO2JKJSO5MMSP4FLIF&response_type=code&redirect_uri=" 
 			. $server_address . "/drivers/oauth.php"
