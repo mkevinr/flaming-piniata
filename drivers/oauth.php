76692777441d33efe49eba1f3f5e43cf33d4e5c3
unknown <kevinm6400@gmail.com>
2013-03-15 03:10:24 -0600

    a

--- drivers/oauth.php
+++ drivers/oauth.php
@@ -1,30 +1,20 @@
 <?php
 
   $server_address = file_get_contents("../server_address");
-  echo "<p>hello</p>";
   session_start();
+  
   if(strlen($_REQUEST['code']) <= 0){
 
-	echo "<p>hello 2</p>";
-    //$_SESSION['create_username'] = $_REQUEST['username'];
-	
-	echo $server_address;
-	
 	$redirect_location = "Location: https://foursquare.com/oauth2/authenticate?"
 			. "client_id=3B53D2V4SEVOHI1R5LNL1H50N4400SQO2JKJSO5MMSP4FLIF&response_type=code&redirect_uri=" 
 			. "http://" . $server_address . "/drivers/oauth.php";
 			
-	echo "<br>" . $redirect_location;
-
     header($redirect_location);
-	
-	echo "hello 4";
   }
   else{
 
     session_start();
 
-	echo "<p>hello 3</p>";
     $ch = curl_init("https://foursquare.com/oauth2/access_token"
     . "?client_id=3B53D2V4SEVOHI1R5LNL1H50N4400SQO2JKJSO5MMSP4FLIF"
     . "&client_secret=J52ZJITMDYABWYUWIIQB5WPDQ4I3DJP5GJBDZLUJRB3CMDY5"
@@ -35,22 +25,15 @@
     curl_setopt($ch, CURLOPT_HEADER, 0);
     $access_token = curl_exec($ch);
 	curl_close($ch);
-	//echo "<br><p>first access token: " . $access_token . "</p>";
-	echo "first access token: ";
-	var_dump($access_token);
     $access_token = json_decode($access_token);
-	var_dump($access_token);
-	//echo "<br><p>second access token: " . $access_token . "</p>";
     $access_token = $access_token->access_token;
-	var_dump($access_token);
-	//echo "<br><p>third access token: " . $access_token . "</p>";
 	
 	$con = mysql_connect("localhost","root","altair8");
     mysql_select_db("driver_site", $con);	
 
     $sql = "UPDATE DRIVERS SET four_square_auth_token='" . $access_token .
 		"' WHERE username='" . $_SESSION['create_username'] . "'";
-	var_dump($sql);
+
     if (!mysql_query($sql,$con))
     {
       die('Error: ' . mysql_error() . " sql: " . $sql);
