387c0d33cc017e75e0c057fad280730399732f46
unknown <kevinm6400@gmail.com>
2013-03-15 01:30:03 -0600

    a

--- drivers/oauth.php
+++ drivers/oauth.php
@@ -7,7 +7,7 @@
 	
 	$server_address = file_get_contents("../server_address");
 	
-	print("server_address: " . server_address);
+	echo "server_address: " . server_address;
 	
 	$redirect_location = "Location: https://foursquare.com/oauth2/authenticate?"
 			. "client_id=3B53D2V4SEVOHI1R5LNL1H50N4400SQO2JKJSO5MMSP4FLIF&response_type=code&redirect_uri=" 
