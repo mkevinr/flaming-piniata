0e389000d13adc54a3b48c099a508e22416a700a
unknown <kevinm6400@gmail.com>
2013-03-15 00:56:50 -0600

    adding updated scripts

--- drivers/create_account.php
+++ drivers/create_account.php
@@ -5,15 +5,37 @@
 <b>Create Account:</b>
 <br><br>
 <?php
-
-  $con = mysql_connect("localhost","root","altair8");
-
-  mysql_select_db("master", $con);
-
+	
+  function getGUID(){
+    if (function_exists('com_create_guid')){
+        return com_create_guid();
+    }else{
+        mt_srand((double)microtime()*10000);//optional for php 4.2.0 and up.
+        $charid = strtoupper(md5(uniqid(rand(), true)));
+        $hyphen = chr(45);// "-"
+        $uuid = chr(123)// "{"
+            .substr($charid, 0, 8).$hyphen
+            .substr($charid, 8, 4).$hyphen
+            .substr($charid,12, 4).$hyphen
+            .substr($charid,16, 4).$hyphen
+            .substr($charid,20,12)
+            .chr(125);// "}"
+        return $uuid;
+    }
+  }
+  
   if(strlen($_REQUEST['username']) > 0){
+  
+	$server_address = file_get_contents("../server_address");
+	$esl = $server_address . "/drivers/input.php?" . getGUID();
+		
+    $con = mysql_connect("localhost","root","altair8");
 
-    $sql = "INSERT INTO USERS (username, password, privileges)"
-	. " VALUES ('" . $_REQUEST['username'] . "', '', 'driver')";
+    mysql_select_db("driver_site", $con);	
+
+    $sql = "INSERT INTO DRIVERS (username, phone_number, esl)"
+		. " VALUES ('" . $_REQUEST['username'] . "', '" . $_REQUEST['phone_number'] . "', '"
+		. $esl . "')";
 
     if (!mysql_query($sql,$con))
     {
@@ -23,26 +45,7 @@
     $sql = "SELECT id FROM USERS WHERE username='" . $_REQUEST['username'] 
            . "'";
 
-    mysql_query($sql,$con);
-
-    $result = mysql_query($sql, $con);
-
-    if (!$result)
-    {
-      die('Error: ' . mysql_error() . " sql: " . $sql);
-    }
-
-    $row = mysql_fetch_array($result);
-    $id = $row['id'];
-
-    $sql = "INSERT INTO DRIVERS VALUES (" . $id . ",'')";
-
-    if (!mysql_query($sql,$con))
-    {
-      die('Error: ' . mysql_error() . " sql: " . $sql);
-    }
-
-    header("Location: /index.php");
+    header("Location: /drivers/oauth.php");
   } 
 
   if($_REQUEST['failed'] === true){
@@ -53,6 +56,7 @@
 
 <form action="/create_account.php" method="POST">
 User name: <input type="text" name="username"><br>
+Phone number: <input type="text" name="phone_number"><br>
 <input type="submit" value="Submit">
 </form>
 <body>
