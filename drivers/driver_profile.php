9f1d6208c035e9cf1291deadfac60241a2e47e41
Kevin Murdock <kevinm6400@gmail.com>
2013-03-17 12:42:30 -0600

    a

--- drivers/driver_profile.php
+++ drivers/driver_profile.php
@@ -2,21 +2,7 @@
 
   session_start();
 
-  $con = mysql_connect("localhost","root","altair8");
-  mysql_select_db("driver_site",$con);
-  $sql = "SELECT id FROM USERS WHERE username='" . $_SESSION['username']
-	. "'";
-  $result = mysql_query($sql);
-
-  if(!$result){
-
-    die("error: " . mysql_error() . "sql: " . $sql);
-  }
-
-  $row = mysql_fetch_array($result);
-  $user_id = $row['id'];
-
-  $sql = "SELECT esl, phone_number FROM DRIVERS WHERE user_id=" . $user_id;
+  $sql = "SELECT esl,phone_number,latitude,longitude FROM DRIVERS WHERE username=" . $_SESSION['username'];
   $result = mysql_query($sql);
 
   if(!$result){
@@ -27,6 +13,8 @@
   $row = mysql_fetch_array($result);
   $esl = $row['esl'];
   $phone_number = $row['phone_number'];
+  $latitude = $row['latitude'];
+  $longitude = $row['longitude'];
 
   if(strlen($_REQUEST['phone_number']) > 0){
 
@@ -42,11 +30,11 @@
     mysql_close($con);
   }
 
-  print("Your esl: " . $esl . "<br>);
-  print("Current Location: Latitude: " . $latitude . " longitude: " . longitude);
+  print("Your esl: " . $esl . "<br>");
+  print("Current Location: Latitude: " . $latitude . " longitude: " . $longitude);
   print("<form action=\"/driver_profile.php\" method=\"POST\">");
   print("Phone number: <input type=\"text\" name=\"phone_number\" size=75 value="
-	. $esl . "><br>");
+	. $phone_number . "><br>");
   print("<input type=\"submit\" value=\"Save\">");
 ?>
 
