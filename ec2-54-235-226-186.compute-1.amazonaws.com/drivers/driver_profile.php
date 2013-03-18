9ebc6efbff59636a3af3db1e16476f6333ba88bd
Kevin Murdock <kevinm6400@gmail.com>
2013-03-18 05:58:52 -0600

    a

--- ec2-54-235-226-186.compute-1.amazonaws.com/drivers/driver_profile.php
+++ ec2-54-235-226-186.compute-1.amazonaws.com/drivers/driver_profile.php
@@ -102,9 +102,9 @@
 	else{
 		print("gets here 2");
 		print("<br/><br/>");
-		print("<form action=\"https://" . $server_address . "/drivers/driver_profile.php?add=" . $_REQUEST['driver_id'] 
+		print("<form action=\"https://" . $server_address . "/drivers/driver_profile.php?add=true&driver_id=" . $_REQUEST['driver_id'] 
 				. "\" method=\"POST\">");
-		print("<input type=\"submit\" value=\"Add\">");
+		print("<input type=\"submit\" value=\"Add\"></form>");
 	}
 	
 	mysql_close($con);
