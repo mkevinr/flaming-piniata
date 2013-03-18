517d5dbbd770a4ec4ec2ef4c55330ba618ab1d6f
Kevin Murdock <kevinm6400@gmail.com>
2013-03-18 16:35:26 -0600

    a

--- ec2-54-235-226-186.compute-1.amazonaws.com/flower_shops/index.php
+++ ec2-54-235-226-186.compute-1.amazonaws.com/flower_shops/index.php
@@ -6,7 +6,7 @@
 	$server_address = file_get_contents("../server_address");
 	echo "<a href=\"https://" . $server_address . "/flower_shops/login.php\"><b>Login</b></a><br>";
 	echo "<a href=\"https://" . $server_address . "/flower_shops/create_account.php\"><b>Create Account</b></a><br>";
-	echo "<a href=\"https://" . $server_address . "/flower_shops/users.php\"><b>Flower Shop Profiles</b></a><br>";
+	echo "<a href=\"https://" . $server_address . "/flower_shops/flower_shops.php\"><b>Flower Shop Profiles</b></a><br>";
 	echo "<a href=\"https://" . $server_address . "/flower_shops/logout.php\"><b>Logout</b></a><br>";
 ?>
 </body>
