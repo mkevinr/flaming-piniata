97ee1d59a0fce2cb4f1464ae799abc11a09f9fa9
Kevin Murdock <kevinm6400@gmail.com>
2013-03-18 05:26:33 -0600

    a

--- ec2-54-235-226-186.compute-1.amazonaws.com/drivers/driver_profile.php
+++ ec2-54-235-226-186.compute-1.amazonaws.com/drivers/driver_profile.php
@@ -68,7 +68,7 @@
 		die("error: " . mysql_error() . "sql: " . $sql);
 	}
 	
-	while($row = msql_fetch_array()){
+	while($row = mysql_fetch_array()){
 	
 		$sql = "SELECT * FROM FLOWER_SHOPS WHERE id=" . $row['flower_shop_id'];
 		$result = mysql_query($sql);
