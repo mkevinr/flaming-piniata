9d2a38e761c961e27ad4e2b4b69a6e60b0623b2c
Kevin Murdock <kevinm6400@gmail.com>
2013-03-18 14:57:13 -0600

    a

--- ec2-54-235-226-186.compute-1.amazonaws.com/drivers/logout.php
+++ ec2-54-235-226-186.compute-1.amazonaws.com/drivers/logout.php
@@ -3,7 +3,7 @@
    $server_address = file_get_contents("../server_address");
 
    session_start();
-   $_SESSION['driver_id'] = '';
+   session_unset('driver_id');
 
    header("Location: https://" . $server_address . "/drivers/");
 ?>
