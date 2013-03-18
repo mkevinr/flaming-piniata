de328b17d39cf2c9f8cb993b6164e9a7d4cab8ed
Kevin Murdock <kevinm6400@gmail.com>
2013-03-18 15:06:11 -0600

    a

--- ec2-54-235-226-186.compute-1.amazonaws.com/index.php
+++ ec2-54-235-226-186.compute-1.amazonaws.com/index.php
@@ -3,7 +3,7 @@
 <b>Welcome!<br/><br/>
 <?php
 
-	$server_address = file_get_contents("../server_address");
+	$server_address = file_get_contents("server_address");
 	print("<a href=\"https://" . $server_address . "/drivers/\">Driver Site</a><br/>");
 	print("<a href=\"https://" . $server_address . "/flower_shops/\">Flower Shop Site</a><br/>");
 ?>
