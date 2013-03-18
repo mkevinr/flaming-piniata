2ad74310ca8571ed4f601516d9097b7d2fe36ee7
Kevin Murdock <kevinm6400@gmail.com>
2013-03-18 00:27:09 -0600

    a

--- drivers/create_account.php
+++ drivers/create_account.php
@@ -32,7 +32,7 @@
 	$user_id = $user_info->response->user->id;
 	
 	$sql = "UPDATE DRIVERS SET four_square_user_id=$user_id WHERE id=" . $row['id'];
-	if(!mysql_query($sql, $con);){
+	if(!mysql_query($sql, $con)){
 	
 		die('Error: ' . mysql_error() . " sql: " . $sql);
 	}
