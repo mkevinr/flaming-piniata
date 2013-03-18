499651e6892da8ea3f3f84f132e1b145c095afa7
Kevin Murdock <kevinm6400@gmail.com>
2013-03-18 06:35:39 -0600

    a

--- ec2-54-235-226-186.compute-1.amazonaws.com/drivers/create_account.php
+++ ec2-54-235-226-186.compute-1.amazonaws.com/drivers/create_account.php
@@ -45,36 +45,16 @@
 	header("Location: https://" . $server_address . "/drivers/");
   }
 	
-  function getGUID(){
-    if (function_exists('com_create_guid')){
-        return com_create_guid();
-    }else{
-        mt_srand((double)microtime()*10000);//optional for php 4.2.0 and up.
-        $charid = strtoupper(md5(uniqid(rand(), true)));
-        $hyphen = chr(45);// "-"
-        $uuid = chr(123)// "{"
-            .substr($charid, 0, 8).$hyphen
-            .substr($charid, 8, 4).$hyphen
-            .substr($charid,12, 4).$hyphen
-            .substr($charid,16, 4).$hyphen
-            .substr($charid,20,12)
-            .chr(125);// "}"
-        return $uuid;
-    }
-  }
-  
   if(strlen($_REQUEST['username']) > 0){
   
 	$server_address = file_get_contents("../server_address");
-	$esl = "https://" . $server_address . "/drivers/input.php?" . getGUID();
 		
     $con = mysql_connect("localhost","root","altair8");
 
     mysql_select_db("driver_site", $con);	
 
-    $sql = "INSERT INTO DRIVERS (username, phone_number, driver_esl)"
-		. " VALUES ('" . $_REQUEST['username'] . "', '" . $_REQUEST['phone_number'] . "', '"
-		. $esl . "')";
+    $sql = "INSERT INTO DRIVERS (username, phone_number)"
+		. " VALUES ('" . $_REQUEST['username'] . "', '" . $_REQUEST['phone_number'] . "')";
 
     if (!mysql_query($sql,$con))
     {
--- ec2-54-235-226-186.compute-1.amazonaws.com/drivers/driver_profile.php
+++ ec2-54-235-226-186.compute-1.amazonaws.com/drivers/driver_profile.php
@@ -1,5 +1,23 @@
 <?php
 
+  function getGUID(){
+    if (function_exists('com_create_guid')){
+        return com_create_guid();
+    }else{
+        mt_srand((double)microtime()*10000);//optional for php 4.2.0 and up.
+        $charid = strtoupper(md5(uniqid(rand(), true)));
+        $hyphen = chr(45);// "-"
+        $uuid = chr(123)// "{"
+            .substr($charid, 0, 8).$hyphen
+            .substr($charid, 8, 4).$hyphen
+            .substr($charid,12, 4).$hyphen
+            .substr($charid,16, 4).$hyphen
+            .substr($charid,20,12)
+            .chr(125);// "}"
+        return $uuid;
+    }
+  }
+
   session_start();
   
   $server_address = file_get_contents("../server_address");
@@ -72,19 +90,26 @@
 	
 		print("Name: " . $row['name'] . " Latitude: " . $row['latitude'] . " Longitude: " . $row['longitude']
 			. "<br/>");
-		print("esl: " . $row['esl'] . "<br/><br/>");
+		print("Your esl: https://" . $server_address . "/drivers/input_event.php?" . $row['driver_esl_token'] . "<br/>");
+		print("Flower shop's esl: " . $row['flower_shop_esl'] . "<br/><br/>");
 	}
 	
 	if(array_key_exists('add', $_REQUEST) && $_REQUEST['add'] == true){
 	
+		$driver_esl_token = getGUID();
+		$driver_esl = "https://" . server_address . "/drivers/input_event.php?" . $driver_esl_token;
+		
+		$sql = "INSERT INTO FLOWER_SHOPS (driver_id,driver_esl_token) VALUES (" . $_SESSION['driver_id'] . ",'" . $driver_esl_token . "')";
+	
 		print("<br/><br/>");
+		print("<b>Your esl: https://" . $server_address . "/drivers/input_event.php?" . $row['driver_esl_token'] . "</b><br/>");
 		print("<form action=\"https://" . $server_address . "/drivers/driver_profile.php?save=true&driver_id=" . $_REQUEST['driver_id']
 				. "\" method=\"POST\">");
 		print("<b>Flower shop name: </b><input type=\"text\" name=\"flower_shop_name\" size=50><br>");
 		print("<b>Latitude: </b><input type=\"text\" name=\"flower_shop_latitude\" size=50><br>");
 		print("<b>Longitude: </b><input type=\"text\" name=\"flower_shop_longitude\" size=50><br>");
 		print("<b>esl: </b><input type=\"text\" name=\"flower_shop_esl\" size=50><br>");
-		print("<input type=\"submit\" value=\"Save\">");		
+		print("<input type=\"submit\" value=\"Save\">");
 	}
 	else{
 		print("<br/>");
