a113313275ac828a8ce64d7b4403daf4230f97ce
Kevin Murdock <kevinm6400@gmail.com>
2013-03-17 21:27:13 -0600

    a

--- drivers/users.php
+++ drivers/users.php
@@ -7,7 +7,7 @@
 $con = mysql_connect("localhost","root","altair8");
 mysql_select_db("driver_site", $con);	
 
-$sql = "SELECT id FROM DRIVERS";
+$sql = "SELECT id, username FROM DRIVERS";
 
 $result = mysql_query($sql,$con);
 
@@ -20,7 +20,7 @@ while($row = mysql_fetch_array($result)){
 
 	$row = mysql_fetch_array($result);
 	$user_id = $row['id'];
-	echo "<a href=\"/drivers/driver_profile.php?user_id=" . $user_id . "\">" . $user_id . "</a><br>";
+	echo "<a href=\"/drivers/driver_profile.php?user_id=" . $user_id . "\">" . $row['username'] . "</a><br>";
 }
 ?>
 
