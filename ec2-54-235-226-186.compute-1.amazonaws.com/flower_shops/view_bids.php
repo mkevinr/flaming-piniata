7c6555391338039a07a6c4ad209b0d9c25d5cb97
Kevin Murdock <kevinm6400@gmail.com>
2013-03-20 15:38:11 -0600

    a

--- ec2-54-235-226-186.compute-1.amazonaws.com/flower_shops/view_bids.php
+++ ec2-54-235-226-186.compute-1.amazonaws.com/flower_shops/view_bids.php
@@ -43,7 +43,8 @@
 	
 	session_start();
 
-	$sql = "SELECT username,driver_id FROM BIDS INNER JOIN USERS ON BIDS.driver_id=USERS.id WHERE delivery_id=" . $_REQUEST['delivery_id'];
+	$sql = "SELECT USERS.username,BIDS.driver_id FROM BIDS INNER JOIN USERS ON BIDS.driver_id=USERS.id WHERE delivery_id=" 
+		. $_REQUEST['delivery_id'];
 
 	$result = mysql_query($sql,$con);
 	
