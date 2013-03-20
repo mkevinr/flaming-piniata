45565f15796fa23f2ff733ed8488e3245e24e9db
Kevin Murdock <kevinm6400@gmail.com>
2013-03-20 03:58:32 -0600

    a

--- ec2-54-235-226-186.compute-1.amazonaws.com/flower_shops/flower_shop_profile.php
+++ ec2-54-235-226-186.compute-1.amazonaws.com/flower_shops/flower_shop_profile.php
@@ -79,7 +79,7 @@
 	
 	  if(array_key_exists('save', $_REQUEST)){
 	  
-		$sql = "UPDATE DRIVERS SET driver_id=" . $_SESSION['driver_id'] . ",flower_shop_id=" $_REQUEST['flower_shop_id'] 
+		$sql = "UPDATE DRIVERS SET driver_id=" . $_SESSION['driver_id'] . ",flower_shop_id=" . $_REQUEST['flower_shop_id'] 
 				. ",flower_shop_esl_token='" . $_REQUEST['flower_shop_esl_token'] . "',driver_esl='" . $_REQUEST['driver_esl'] . "'";
 				
 		if(!mysql_query($sql, $con)){
