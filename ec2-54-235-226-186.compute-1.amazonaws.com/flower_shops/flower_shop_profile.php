104e386067fb0b7b519dc153aa9d5329d45d041a
Kevin Murdock <kevinm6400@gmail.com>
2013-03-20 14:22:30 -0600

    a

--- ec2-54-235-226-186.compute-1.amazonaws.com/flower_shops/flower_shop_profile.php
+++ ec2-54-235-226-186.compute-1.amazonaws.com/flower_shops/flower_shop_profile.php
@@ -107,7 +107,6 @@
 		
 		print("<br/><b>Your Username:</b> " . $driver_username);
 		print("<br/><b>Flower Shop's esl:</b> " . "https://" . $server_address . "/flower_shops/event_input.php?esl_token=" . $flower_shop_esl_token);
-		print("requeset['flower_shop_id']: ->" . $_REQUEST['flower_shop_id']. "<-");
 		print("<form action=\"https://" . $server_address . "/flower_shops/flower_shop_profile.php?flower_shop_id=" . $_REQUEST['flower_shop_id']
 				. "&flower_shop_esl_token=" . $flower_shop_esl_token . "&save=true" . "\" method=\"POST\">");
 		print("<br/>Your esl: </b><input type=\"text\"/>");
