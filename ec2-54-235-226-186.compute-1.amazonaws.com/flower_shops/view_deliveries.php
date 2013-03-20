02ff7dcc433d0c9d95d99f1b93bbb23c10b16b2d
Kevin Murdock <kevinm6400@gmail.com>
2013-03-20 14:52:40 -0600

    flower shop profile working

--- ec2-54-235-226-186.compute-1.amazonaws.com/flower_shops/view_deliveries.php
+++ ec2-54-235-226-186.compute-1.amazonaws.com/flower_shops/view_deliveries.php
@@ -3,13 +3,13 @@
 
 <?php
 
+	session_start();
+
 	$server_address = file_get_contents("../server_address");
 	print("<a href=\"https://" . $server_address . "/flower_shops/\"><b>Home</b></a><br/>");
 	print("<a href=\"https://" . $server_address . "/flower_shops/flower_shop_profile.php?" . $_SESSION['flower_shop_id'] . "\"><b>Back</b></a><br/><br/>");
 	
 	print("<b>Deliveries:</b><br/><br/>");
-
-	session_start();
 
     $con = mysql_connect("localhost", "root", "altair8");
     mysql_select_db("flower_shop_site", $con);
