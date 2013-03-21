07f44a222a55fb3f0b09364051fcacb0d16cc118
Kevin Murdock <kevinm6400@gmail.com>
2013-03-21 00:08:30 -0600

    adding a post test

--- ec2-54-235-226-186.compute-1.amazonaws.com/post_test.php
+++ ec2-54-235-226-186.compute-1.amazonaws.com/post_test.php
@@ -1,10 +1,11 @@
 <?php
 
-
-	/*if(array_key_exists('send', $_REQUEST)){
+	
+	if(array_key_exists('send', $_REQUEST)){
+		print("sent: " . $_REQUEST['send'];
 		$request = "hello";
 		
-		$url="https://ec2-54-235-226-186.compute-1.amazonaws.com/post_receive_test.php?test=yes";
+		/*$url="https://ec2-54-235-226-186.compute-1.amazonaws.com/post_receive_test.php?test=yes";
 		$ch = curl_init($url);
 		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $request);
@@ -14,8 +15,9 @@
 			'Content-Length: ' . strlen($request))
 		);
 		curl_exec($ch);
-		print("after sending curl request");
-	}*/
+		print("after sending curl request");*/
+	}
+	print('after if');
 ?>
 <form action="https://ec2-54-235-226-186.compute-1.amazonaws.com/post_test.php?send=true">
 <input type="submit" value="Send"></form>
