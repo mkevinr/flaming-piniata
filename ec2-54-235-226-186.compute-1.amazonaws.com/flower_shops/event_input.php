c009354723c6efba330184ac74566c61f591c086
Kevin Murdock <kevinm6400@gmail.com>
2013-03-21 15:42:20 -0600

    adding a post test

--- ec2-54-235-226-186.compute-1.amazonaws.com/flower_shops/event_input.php
+++ ec2-54-235-226-186.compute-1.amazonaws.com/flower_shops/event_input.php
@@ -61,6 +61,7 @@
 		if(!mysql_query($sql, $con)){
 		
 			file_put_contents("flower_shop_event_input_test", "\ngot_here 15", FILE_APPEND);
+			file_put_contents("flower_shop_event_input_test", "\ngot_here 15: " . mysql_error(), FILE_APPEND);
 			die("error: " . mysql_error() . " sql: " . $sql);
 		}
 		file_put_contents("flower_shop_event_input_test", "\ngot_here 5", FILE_APPEND);
