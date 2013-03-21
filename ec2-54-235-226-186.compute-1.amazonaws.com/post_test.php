e2723b780886a8b052907f159cc586414854830e
Kevin Murdock <kevinm6400@gmail.com>
2013-03-20 20:14:11 -0600

    adding a post test

---
+++ ec2-54-235-226-186.compute-1.amazonaws.com/post_test.php
@@ -0,0 +1,20 @@
+<?php
+
+
+	if(array_key_exists('send', $_REQUEST){
+		$request = "hello";
+		
+		$url="https://ec2-54-235-226-186.compute-1.amazonaws.com/post_receive_test.php"
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $request);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+			'Content-Type: application/json',
+			'Content-Length: ' . strlen($request))
+		);
+		curl_exec($ch);
+	}
+?>
+<form action="https://ec2-54-235-226-186.compute-1.amazonaws.com/poset_receive_test.php?test=yes">
+<input type="submit" value="Send"></form>
