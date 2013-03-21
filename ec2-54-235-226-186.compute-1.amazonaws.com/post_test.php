8ef75a7a862d6f81b9fc0610019d2e32e3cbca29
Kevin Murdock <kevinm6400@gmail.com>
2013-03-21 00:29:24 -0600

    adding a post test

--- ec2-54-235-226-186.compute-1.amazonaws.com/post_test.php
+++ ec2-54-235-226-186.compute-1.amazonaws.com/post_test.php
@@ -7,7 +7,8 @@
 		$request = "hello";
 		
 		//$url="https://ec2-54-235-226-186.compute-1.amazonaws.com/post_receive_test.php?test=yes";
-		$url="http://requestb.in/x8lr1zx8?inspect";
+		//$url="http://requestb.in/x8lr1zx8?inspect";
+		$url="http://requestb.in/14yp02s1";
 		$ch = curl_init($url);
 		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $request);
@@ -16,7 +17,7 @@
 			'Content-Type: application/json',
 			'Content-Length: ' . strlen($request))
 		);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		//curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 		curl_exec($ch);
 		print("after sending curl request");
 	}
