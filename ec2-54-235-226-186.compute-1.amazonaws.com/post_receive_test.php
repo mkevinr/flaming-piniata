0c912fe643172ca801a25f8fe8e5088c997f1acf
Kevin Murdock <kevinm6400@gmail.com>
2013-03-21 00:02:25 -0600

    adding a post test

--- ec2-54-235-226-186.compute-1.amazonaws.com/post_receive_test.php
+++ ec2-54-235-226-186.compute-1.amazonaws.com/post_receive_test.php
@@ -1,6 +1,7 @@
 <?php
 
 	$entityBody = file_get_contents('php://input');
+	file_put_contents("entity body: " . $entityBody);
 	
 	print("<p>" . $entityBody . "/p");
 	print("<p>Request['test']: " . $_REQUEST['test']);
