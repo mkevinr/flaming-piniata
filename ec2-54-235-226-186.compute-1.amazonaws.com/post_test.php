96b97fe90445a94c970557c07fa6ab9d2b64a8cd
Kevin Murdock <kevinm6400@gmail.com>
2013-03-21 00:08:55 -0600

    adding a post test

--- ec2-54-235-226-186.compute-1.amazonaws.com/post_test.php
+++ ec2-54-235-226-186.compute-1.amazonaws.com/post_test.php
@@ -2,7 +2,7 @@
 
 	
 	if(array_key_exists('send', $_REQUEST)){
-		print("sent: " . $_REQUEST['send'];
+		print("sent: " . $_REQUEST['send']);
 		$request = "hello";
 		
 		/*$url="https://ec2-54-235-226-186.compute-1.amazonaws.com/post_receive_test.php?test=yes";
