b6debdebac200d49c2608c2d0148277b8028029d
Kevin Murdock <kevinm6400@gmail.com>
2013-03-21 18:14:42 -0600

    adding a post test

--- ec2-54-235-226-186.compute-1.amazonaws.com/flower_shops/view_bids.php
+++ ec2-54-235-226-186.compute-1.amazonaws.com/flower_shops/view_bids.php
@@ -19,6 +19,7 @@
 			die("error: " . mysql_error() . " sql: " . $sql);
 		}
 		
+		$row = mysql_fetch_array($result);		
 		$sql = "UPDATE DELIVERIES SET assigned_driver_id=" . $row['driver_id'] . ",estimated_delivery_time=" . $row['estimated_delivery_time'];
 		
 		if(!mysql_query($sql, $con)){
