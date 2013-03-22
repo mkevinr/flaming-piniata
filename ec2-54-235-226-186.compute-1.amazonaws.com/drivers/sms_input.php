fcd00952c41b318145ba96013d0c19d245025f21
Kevin Murdock <kevinm6400@gmail.com>
2013-03-22 03:38:51 -0600

    a

--- ec2-54-235-226-186.compute-1.amazonaws.com/drivers/sms_input.php
+++ ec2-54-235-226-186.compute-1.amazonaws.com/drivers/sms_input.php
@@ -1,6 +1,11 @@
 <?php
 
 	file_put_contents("sms_input_test", "was called!");
+	
+	$entityBody = file_get_contents('php://input');
+	
+	file_put_contents("sms_input_test", "\nrequest: " . print_r($_REQUEST), FILE_APPEND);
+	file_put_contents("sms_input_test", "\n" . $entityBody, FILE_APPEND);
 
     //require '/path/to/twilio-php/Services/Twilio.php';
 ?>
