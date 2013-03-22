933ea4a34620035f5e3fc4a3058f02bfff216a0e
Kevin Murdock <kevinm6400@gmail.com>
2013-03-22 12:40:42 -0600

    a

--- ec2-54-235-226-186.compute-1.amazonaws.com/drivers/event_input.php
+++ ec2-54-235-226-186.compute-1.amazonaws.com/drivers/event_input.php
@@ -152,11 +152,15 @@
 	file_put_contents("driver_site_event_input_test", "\ngets to just before sending text message", FILE_APPEND);
 	// send text message to $phone_number
 	require "services-php/Services/Twilio.php";
+	file_put_contents("driver_site_event_input_test", "\ngets to sending text message 1", FILE_APPEND);
 	
 	$accountSID = "ACc4c3a904e660afb30533bdcf81e6e5fe";
+	file_put_contents("driver_site_event_input_test", "\ngets to sending text message 2", FILE_APPEND);
 	$authToken = "38353271ed843f8df4d0aac99db0c233";
+	file_put_contents("driver_site_event_input_test", "\ngets to sending text message 3", FILE_APPEND);
 	
 	$client = new Services_Twilio($accountSID, $authToken);
+	file_put_contents("driver_site_event_input_test", "\ngets to sending text message 4", FILE_APPEND);
 	$sms = $client->account->sms_messages->create("+12086470634", $phone_number, $text_message);
 	file_put_contents("driver_site_event_input_test", "\ngets to end", FILE_APPEND);
 	print("<p><b>after if</b></p>");
