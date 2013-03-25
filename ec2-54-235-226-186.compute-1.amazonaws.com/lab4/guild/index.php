e21a9d954daf9a629e86bac25505e7e94553a7b6
Kevin Murdock <kevinm6400@gmail.com>
2013-03-25 04:46:54 -0600

    Added the index page to the guild site because I forgot to earlier.

---
+++ ec2-54-235-226-186.compute-1.amazonaws.com/lab4/guild/index.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+Welcome to the Driver's Guild site!<br/><br/>
+
+<?php
+
+	$server_address = file_get_contents("../server_address");
+	
+	print("<a href=\"https://" . $server_address . "/lab4/guild/register.php\">Register</a>";
+	
+?>
+
+</html>
+</body>
