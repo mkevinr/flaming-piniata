4e3a8991184989a5a71e17c80a9ee6976ae934f0
Kevin Murdock <kevinm6400@gmail.com>
2013-03-25 05:17:03 -0600

    a

--- ec2-54-235-226-186.compute-1.amazonaws.com/lab4/guild/register.php
+++ ec2-54-235-226-186.compute-1.amazonaws.com/lab4/guild/register.php
@@ -93,7 +93,7 @@
 	
 		$universal_id = getGUID();
 		$driver_esl_token = getGUID();
-		print("<b>Your universal identifier:</b> " . $universal_identifier . "<br/>");
+		print("<b>Your universal identifier:</b> " . $universal_id . "<br/>");
 		print("<b>Guild's esl for you:</b> https://" . $server_address . "/lab4/guild/event_input.php?esl_token=" . $driver_esl_token . "<br/>");
 		print("<form action=\"https://" . $server_address . "/lab4/guild/register.php?user_type=driver&universal_id="
 				. $universal_id . "&guild_esl_token=" . $driver_esl_token . "\" method=\"POST\">");
